ObjectBoxPC/irvingt

commit 9945213f5d044faadc55ad1bce693353329a06e0

Arrange functions in alphabetical order

irvingt-real.c
@@ -25,15 +25,15 @@ void DumpRegs_Real(int eip, int efl, int edi, int esi, int ebp, int esp, int ebx
 }
 #endif
 
-void WaitMsg_Real(void) {
-	puts("Press Enter to continue...");
-	getchar();
-}
-
 void ExitProcess(int status) {
 	puts("Exit");
 	exit(status);
-}
+
+
+void WaitMsg_Real(void) {
+	puts("Press Enter to continue...");
+	getchar();
+}}
 
 #ifndef IRVINGT_HAVE_ALONG
 void WriteDec_Real(int x) {

commit 5ca30d78d75081de3c982f05ddad85288ef52aaf

Fix braces
This problem was introduced in 9945213f5d044faadc55ad1bce693353329a06e0

irvingt-real.c
@@ -28,12 +28,12 @@ void DumpRegs_Real(int eip, int efl, int edi, int esi, int ebp, int esp, int ebx
 void ExitProcess(int status) {
 	puts("Exit");
 	exit(status);
-
+}
 
 void WaitMsg_Real(void) {
 	puts("Press Enter to continue...");
 	getchar();
-}}
+}
 
 #ifndef IRVINGT_HAVE_ALONG
 void WriteDec_Real(int x) {